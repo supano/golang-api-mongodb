19881728021254b2c7911d97d9de8fb7e31e7cc4
Supachai Suthikeeree <supano1995@gmail.com>
2018-11-30 09:17:12 +0700

    golang api CRUD with mongodb driver

---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		e.Logger.Fatal(err)
+		return
+	}
+
+	h := &handler{
+		m: session,
+	}
+
+	e.Use(middleware.Logger())
+	e.GET("/api/users", h.list)
+	e.GET("/api/users/:id", h.view)
+	e.POST("/api/users", h.create)
+	e.PUT("/api/users/:id", h.update)
+	e.PATCH("/api/users/:id", h.update)
+	e.DELETE("/api/users/:id", h.delete)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type handler struct {
+	m *mgo.Session
+}
+
+type user struct {
+	ID       bson.ObjectId `json:"id" bson:"_id"`
+	Email    string        `json:"email" bson:"email"`
+	Password string        `json:"password" bson:"password"`
+	Fname    string        `json:"fname" bson:"fname"`
+	Lname    string        `json:"lname" bson:"lname"`
+}
+
+func (h *handler) create(c echo.Context) error {
+	session := h.m.Copy()
+	defer session.Close()
+
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	u.ID = bson.NewObjectId()
+
+	collection := session.Copy().DB("golang").C("users")
+	if err := collection.Insert(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, u)
+}
+
+func (h *handler) list(c echo.Context) error {
+	session := h.m.Copy()
+	defer session.Close()
+
+	collection := session.Copy().DB("golang").C("users")
+	var userlist []user
+	if err := collection.Find(nil).All(&userlist); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, userlist)
+}
+
+func (h *handler) view(c echo.Context) error {
+	session := h.m.Copy()
+	defer session.Close()
+	collection := session.Copy().DB("golang").C("users")
+	id := bson.ObjectIdHex(c.Param("id"))
+	var u user
+	if err := collection.FindId(id).One(&u); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, u)
+}
+
+func (h *handler) update(c echo.Context) error {
+	session := h.m.Copy()
+	defer session.Close()
+	collection := session.Copy().DB("golang").C("users")
+	id := bson.ObjectIdHex(c.Param("id"))
+
+	n := new(user)
+	c.Bind(n)
+
+	var u user
+	if err := collection.FindId(id).One(&u); err != nil {
+		return err
+	}
+
+	if err := collection.UpdateId(id, n); err != nil {
+		return err
+	}
+
+	var nu user
+	if err := collection.FindId(id).One(&nu); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, nu)
+
+}
+
+func (h *handler) delete(c echo.Context) error {
+	sesstion := h.m.Copy()
+	defer sesstion.Close()
+	collection := sesstion.Copy().DB("golang").C("users")
+	id := bson.ObjectIdHex(c.Param("id"))
+
+	if err := collection.RemoveId(id); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, true)
+}
